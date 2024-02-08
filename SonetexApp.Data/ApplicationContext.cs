6c3d195faad22c8d2001d90674cb6f37443783a9
Ruzimurod2003 <Ruzimurodabdunazarov2003@mail.ru>
2024-02-08 20:35:02 +0500

    added dbContext section for SonetexApp.Data project

---
+++ SonetexApp.Data/ApplicationContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SonetexApp.Models;
+
+namespace SonetexApp.Data;
+
+public class ApplicationContext : DbContext
+{
+    public List<Catalog> Catalogs { get; set; }
+    public List<Product> Products { get; set; }
+    public List<Certificate> Certificates { get; set; }
+    public List<Manufacturer> Manufacturers { get; set; }
+    public List<Partner> Partners { get; set; }
+    public List<Recommendation> Recommendations { get; set; }
+    public List<Specialist> Specialists { get; set; }
+    public List<State> States { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("Data Source = SonetexDB.db");
+    }
+}
