c26891f3db6df6816187c327f8d629ade2873db1
Ruzimurod2003 <Ruzimurodabdunazarov2003@mail.ru>
2024-02-08 19:07:12 +0500

    added models for initial project structure

---
+++ SonetexApp.Models/Certificate.cs
@@ -0,0 +1,10 @@
+﻿namespace SonetexApp.Models;
+public class Certificate                        // Сертификаты 
+{
+    public int Id { get; set; }
+    public string Name { get; set; }            // Hазвание 
+    public string NameRussian { get; set; }     // Русский нaзвание 
+    public string NameEnglish { get; set; }     // Английский название 
+    public string NameUzbek { get; set; }       // Узбекский название 
+    public string Description { get; set; }     // Описание 
+}
---
+++ SonetexApp.Models/Manufacturer.cs
@@ -0,0 +1,11 @@
+﻿namespace SonetexApp.Models;
+public class Manufacturer                       // Производители 
+{
+    public int Id { get; set; }
+    public string Name { get; set; }            // Hазвание 
+    public string NameRussian { get; set; }     // Русский нaзвание 
+    public string NameEnglish { get; set; }     // Английский название 
+    public string NameUzbek { get; set; }       // Узбекский название 
+    public string Description { get; set; }     // Описание 
+    public List<Catalog> Catalogs { get; set; } = new List<Catalog>();
+}
